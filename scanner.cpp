1cb825e0ac1575979d59135f6163aaae35f0993b
Benjamin Pachev <benjaminpachev@gmail.com>
2016-01-22 18:01:31 -0700

    Work on abstracting into .h files, automated testing.

--- scanner.cpp
+++ scanner.cpp
@@ -1,23 +1,5 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <ctype.h>
+#include "token.h"
 
-using namespace std;
-
-enum token {COMMA,PERIOD,Q_MARK,LEFT_PAREN,RIGHT_PAREN,COLON,
-  COLON_DASH,SCHEMES,FACTS,RULES,QUERIES,ID,STRING,END_FILE};
-
-enum State {QUOTE_,MASTER_,ID_,COLON_,COMMENT_,ERR_}; // to not be confused with the token type
-
-struct tok
-{
-  int line;
-  token t; //type
-  string s; //token string
-};
 
 struct state_info
 {
--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-for i in in*.txt
+touch o.txt
+for i in $1/in*.txt
 do
   echo $i
-  valgrind --leak-check=full --error-exitcode=1 2>val_err.txt ./lab1 $i o.txt
+  valgrind --leak-check=full --error-exitcode=1 2>val_err.txt ./lab1 $1/$i o.txt
   rc=$?; if [[ $rc != 0 ]]; then echo "Valgrind failed "; fi
-  diff o.txt out${i:2:20}
+  diff o.txt $i/out${i:2:20}
 
 done
