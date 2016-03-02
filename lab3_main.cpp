14d211761b61b0902abbc107f68d3c391dd2fa25
Benjamin Alexander Pachev <benjaminpachev@gmail.com>
2016-03-01 17:19:42 -0700

    Added missing file.

---
+++ lab3_main.cpp
@@ -0,0 +1,16 @@
+#include "interpreter.h"
+
+int main (int argc, char** argv)
+{
+  if (argc < 3)
+  {
+    cout << "Usage infile outfile." << endl;
+    return 0;
+  }
+
+  ofstream out;
+  out.open(argv[2]);
+
+  Interpreter i = Interpreter();
+  i.run(argv[1], out, PROJ3);
+}
